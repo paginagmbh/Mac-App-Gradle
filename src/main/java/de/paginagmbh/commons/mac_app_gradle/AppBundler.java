0cd62e68eca634d70e54269d63df780e0d7b25fc
Hans Schülein <hans.schuelein@pagina.gmbh>
2024-02-22 07:34:35 +0100

    Added NSHighResolutionCapable flag to Info.plist

--- src/main/java/de/paginagmbh/commons/mac_app_gradle/AppBundler.java
+++ src/main/java/de/paginagmbh/commons/mac_app_gradle/AppBundler.java
@@ -223,6 +223,7 @@ public class AppBundler extends DefaultTask {
     infoPlist.createEntry("CFBundleDevelopmentRegion", developmentRegion);
     infoPlist.createEntry("NSHumanReadableCopyright", copyright);
     infoPlist.createEntry("CFBundleIconFile", iconName);
+    infoPlist.createEntry("NSHighResolutionCapable", true);
     // Settings for files that can be opened in said app.
     if (viewableDocumentTypes != null)
       infoPlist.documentTypes(
